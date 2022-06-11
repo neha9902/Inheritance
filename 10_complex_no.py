0426fc6c4750f858048c7532243a70840df37177
Neha Bharti <55710527+neha9902@users.noreply.github.com>
2022-06-12 00:43:47 +0530

    Add files via upload

---
+++ 10_complex_no.py
@@ -0,0 +1,18 @@
+class Complex:
+    def __init__(self,r,i):
+        self.real=r;
+        self.imaginary=i;
+    def __add__(self,c):
+        return Complex(self.real+c.real,self.imaginary+c.imaginary)
+
+    def __mul__(self,c):
+        mulreal = self.real*c.real - self.imaginary*c.imaginary
+        mulimaginary =  self.real*c.imaginary + self.imaginary*c.real
+        return Complex(mulreal,mulimaginary)   
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"     
+
+c1=Complex(2,3)
+c2=Complex(5,6)
+print(c1+c2)
+print(c1*c2)           
